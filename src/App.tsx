cdfaf204cddfbda045d37824d4495a83a7943a18
fedjes <salvados78@mail.ru>
2023-09-17 14:23:50 +0300

    dont use strictMode!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState<number>(0);
+
+  useEffect(() => {
+    let localValue = localStorage.getItem('counter')
+    console.log({ localValue })
+    if (localValue) {
+      let newVal = JSON.parse(localValue)
+      setValue(newVal)
+    }
+  }, [])
+
+
+
+  useEffect(() => {
+    console.log({ value })
+    localStorage.setItem('counter', JSON.stringify(value))
+  }, [value])
+
+
+
+
+  const incHandler = () => {
+    setValue(value + 1)
+  }
+  // const setLocalStorageHandler = () => {
+  //   localStorage.setItem('couner', JSON.stringify(value))
+  // }
+
+  // const getFromLocalStorageHandler = () => {
+  // let valueLocal =  localStorage.getItem('couner')
+  // if(valueLocal) {
+  //   let changedValue = JSON.parse(valueLocal)
+  //   setValue(changedValue)
+  // }
+  // }
+
+  // const clearLocalStorageHandler = () => {
+  //   localStorage.clear()
+  //   setValue(0)
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+      <button onClick={incHandler}>inc</button>
+      {/* <button onClick={setLocalStorageHandler}>setLocal</button>
+      <button onClick={getFromLocalStorageHandler}>getLocal</button>
+      <button onClick={clearLocalStorageHandler}>clear</button> */}
     </div>
   );
 }
